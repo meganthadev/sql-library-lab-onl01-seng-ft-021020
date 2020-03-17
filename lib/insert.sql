76daf5857c9ab4328bf976282165abc4b96e1cfa
Megan Horsfield <meganthadev@gmail.com>
2020-03-17 21:08:20 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,30 @@
+INSERT INTO series (id, title, author_id, subgenre_id) VALUES ()
+INSERT INTO series (id, title, author_id, subgenre_id) VALUES ()
+
+INSERT INTO subgenres (id, name) VALUES ()
+INSERT INTO subgenres (id, name) VALUES ()
+
+INSERT INTO authors
+INSERT INTO authors
+
+INSERT INTO books
+INSERT INTO books
+INSERT INTO books
+INSERT INTO books
+INSERT INTO books
+INSERT INTO books
+
+INSERT INTO characters
+INSERT INTO characters
+INSERT INTO characters
+INSERT INTO characters
+INSERT INTO characters
+INSERT INTO characters
+INSERT INTO characters
+INSERT INTO characters
+
+INSERT INTO character_books
+INSERT INTO character_books
+INSERT INTO character_books
+INSERT INTO character_books
+

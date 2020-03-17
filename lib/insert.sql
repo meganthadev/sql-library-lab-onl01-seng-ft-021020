6574ef6d13ab97cb3c5620c1a57a2e39a6581bbd
Megan Horsfield <meganthadev@gmail.com>
2020-03-17 21:16:02 +0000

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -4,27 +4,27 @@ INSERT INTO series (id, title, author_id, subgenre_id) VALUES ()
 INSERT INTO subgenres (id, name) VALUES ()
 INSERT INTO subgenres (id, name) VALUES ()
 
-INSERT INTO authors
-INSERT INTO authors
+INSERT INTO authors (id, name) VALUES ()
+INSERT INTO authors (id, name) VALUES ()
 
-INSERT INTO books
-INSERT INTO books
-INSERT INTO books
-INSERT INTO books
-INSERT INTO books
-INSERT INTO books
+INSERT INTO books (id, title, year, series_id) VALUES ()
+INSERT INTO books (id, title, year, series_id) VALUES ()
+INSERT INTO books (id, title, year, series_id) VALUES ()
+INSERT INTO books (id, title, year, series_id) VALUES ()
+INSERT INTO books (id, title, year, series_id) VALUES ()
+INSERT INTO books (id, title, year, series_id) VALUES ()
 
-INSERT INTO characters
-INSERT INTO characters
-INSERT INTO characters
-INSERT INTO characters
-INSERT INTO characters
-INSERT INTO characters
-INSERT INTO characters
-INSERT INTO characters
+INSERT INTO characters (id, name, motto, species, author_id) VALUES ()
+INSERT INTO characters (id, name, motto, species, author_id) VALUES ()
+INSERT INTO characters (id, name, motto, species, author_id) VALUES ()
+INSERT INTO characters (id, name, motto, species, author_id) VALUES ()
+INSERT INTO characters (id, name, motto, species, author_id) VALUES ()
+INSERT INTO characters (id, name, motto, species, author_id) VALUES ()
+INSERT INTO characters (id, name, motto, species, author_id) VALUES ()
+INSERT INTO characters (id, name, motto, species, author_id) VALUES ()
 
-INSERT INTO character_books
-INSERT INTO character_books
-INSERT INTO character_books
-INSERT INTO character_books
+INSERT INTO character_books (id, book_id, character_id) VALUES ()
+INSERT INTO character_books (id, book_id, character_id) VALUES ()
+INSERT INTO character_books (id, book_id, character_id) VALUES ()
+INSERT INTO character_books (id, book_id, character_id) VALUES ()
 

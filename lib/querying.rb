7117241602f7f5ef92a3c23884eaff5f3f0f5857
Megan Horsfield <meganthadev@gmail.com>
2020-03-17 23:22:25 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -3,17 +3,17 @@ def select_books_titles_and_years_in_first_series_order_by_year
   FROM books 
   WHERE series_id = 1
   ORDER by year;"
-end	end
+end	
 
 
-def select_name_and_motto_of_char_with_longest_motto	def select_name_and_motto_of_char_with_longest_motto
+def select_name_and_motto_of_char_with_longest_motto
   "SELECT name, motto
   FROM characters
   ORDER BY LENGTH(motto) DESC LIMIT 1;"
 end
 
 
-def select_value_and_count_of_most_prolific_species	def select_value_and_count_of_most_prolific_species
+def select_value_and_count_of_most_prolific_species	
   "SELECT species, COUNT(species)
   FROM characters
   GROUP BY species ORDER BY COUNT(species) DESC LIMIT 1;"
@@ -44,10 +44,10 @@ def select_series_title_with_most_human_characters
 end	
 
 
-def select_character_names_and_number_of_books_they_are_in	def select_character_names_and_number_of_books_they_are_in
+def select_character_names_and_number_of_books_they_are_in	
   "SELECT characters.name, COUNT(*) as book_count
   FROM characters
   JOIN character_books ON character_books.character_id = characters.id
   GROUP BY characters.name
   ORDER BY book_count DESC;"
-end	end
+end	
